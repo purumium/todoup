46a648d4b23c6a01bd537c90dee6b6b8658b8fc4
Yohan Park <yohan9569@gmail.com>
2024-08-07 19:55:48 +0900

    refactor: split AsideCompo.vue into ProfileCompo.vue and TodayTodo.vue

--- Todoup_fe/src/components/MainCompo.vue
+++ Todoup_fe/src/components/MainCompo.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="d-flex w-100" style="height: 100vh">
-    <aside-compo></aside-compo>
+    <!-- <aside-compo></aside-compo> -->
+    <aside>
+      <profile-compo />
+      <today-todo />
+    </aside>
     <modal-compo class="test"></modal-compo>
     <div class="view-wrapper">
       <router-view></router-view>
@@ -10,11 +14,18 @@
 </template>
 
 <script>
-import AsideCompo from './common/AsideCompo.vue';
+// import AsideCompo from './common/AsideCompo.vue';
+import ProfileCompo from './common/ProfileCompo.vue';
+import TodayTodo from './common/TodayTodo.vue';
 import ModalCompo from './modal/ModalCompo.vue';
 
 export default {
-  components: { AsideCompo, ModalCompo },
+  components: {
+    // AsideCompo,
+    ModalCompo,
+    ProfileCompo,
+    TodayTodo,
+  },
 };
 </script>
 
