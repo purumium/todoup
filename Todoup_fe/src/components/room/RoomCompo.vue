9a79c81a716ff0e03236b4c0ffaae55b3b3c22a1
Yohan Park <yohan9569@gmail.com>
2024-08-07 17:57:44 +0900

    feat: add room related components

---
+++ Todoup_fe/src/components/room/RoomCompo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+    <div class="room-btn-group">
+      <router-link to="/avatarroom">
+        <button>AVATARROOM</button>
+      </router-link>
+      <router-link to="/report">
+        <button>REPORT</button>
+      </router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RoomCompo',
+};
+</script>
+
+<style scoped>
+.room-btn-group {
+  display: flex;
+  justify-content: flex-start;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ Todoup_fe/src/components/room/report/FirstReport.vue
@@ -0,0 +1,17 @@
+<!-- Report1.vue -->
+<template>
+  <div>
+    <h2>Report 1</h2>
+    <!-- 리포트 1 내용 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstReport',
+};
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
