9f0de0d6a74e53e622b532473897705f2b8410dc
pureum <purumium@gmail.com>
2024-08-07 10:15:08 +0900

    1. asideCompo 내용 추가  2. todo, diary 캘린더 버튼 추가

--- Todoup_fe/src/router/route.js
+++ Todoup_fe/src/router/route.js
@@ -1,13 +1,34 @@
-import MainCompo from '@/components/MainCompo.vue';
 import MainWrapperCompo from '@/components/MainWrapperCompo.vue';
+import MainCompo from '@/components/MainCompo.vue';
+import ViewCompo from '@/components/ViewCompo.vue';
+import TodoCalendar from '@/components/TodoCalendar.vue';
+import DiaryCalendar from '@/components/DiaryCalendar.vue';
+
 export default [
   {
     path: '/',
     component: MainWrapperCompo,
     children: [
       {
-        path: '/',
+        path: '',
         component: MainCompo,
+        children: [
+          {
+            path: '',
+            component: ViewCompo,
+            children: [
+              {
+                path: '',
+                component: TodoCalendar,
+                alias: 'todo',
+              },
+              {
+                path: 'diary',
+                component: DiaryCalendar,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
