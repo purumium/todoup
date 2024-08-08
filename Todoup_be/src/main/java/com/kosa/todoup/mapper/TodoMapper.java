66fe445e47ce6368d7c42ee9cbba6e5b71a2bd51
이연수 <dldustn2040@gmail.com>
2024-08-09 08:58:54 +0900

    feat: 할일목록조회

--- Todoup_be/src/main/java/com/kosa/todoup/mapper/TodoMapper.java
+++ Todoup_be/src/main/java/com/kosa/todoup/mapper/TodoMapper.java
@@ -2,8 +2,15 @@ package com.kosa.todoup.mapper;
 
 import com.kosa.todoup.dto.TodoDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface TodoMapper {
     void insertTodo(TodoDTO todoDTO);
+
+    List<TodoDTO> selectTodosByDate(@Param("date") String date);
+
+    TodoDTO selectTodoById(@Param("todoId") long todoId);
 }
