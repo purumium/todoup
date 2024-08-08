c623fdaff4ba6db8295ac4debf830dccd49d003a
Yohan Park <yohan9569@gmail.com>
2024-08-08 15:49:04 +0900

    feat: implemented basic layout for AvatarRoom

--- Todoup_fe/src/components/room/avatar/AvatarRoom.vue
+++ Todoup_fe/src/components/room/avatar/AvatarRoom.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h3>AvatarRoom</h3>
-    <back-ground />
-    <avatar-character />
-    <guest-book />
+    <back-ground>
+      <template #avatar>
+        <div class="avatar-wrapper" :class="{ 'is-friend-room': !isMyRoom }">
+          <avatar-character class="avatar" />
+          <avatar-character v-if="!isMyRoom" class="avatar friend-avatar" />
+        </div>
+      </template>
+      <template #guestbook>
+        <guest-book />
+      </template>
+    </back-ground>
   </div>
 </template>
 
@@ -11,12 +19,53 @@
 import AvatarCharacter from './AvatarCharacter.vue';
 import BackGround from './BackGround.vue';
 import GuestBook from './GuestBook.vue';
+
 export default {
   name: 'AvataRoom',
   components: { BackGround, AvatarCharacter, GuestBook },
+  data() {
+    return {
+      isMyRoom: false, // 임시로 내 방 여부를 설정 (내 방이면 true, 친구 방이면 false)
+    };
+  },
+  /*
+  props: {
+    ownerId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    isMyRoom() {
+      return this.$store.state.user.id === this.ownerId;
+    },
+  }
+  */
 };
 </script>
 
 <style scoped>
-/* 스타일 정의 */
+.avatar-wrapper {
+  display: flex;
+  justify-content: center; /* 기본 중앙 정렬 */
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  /* border: 1px solid blue; 구분을 위해 테두리 추가 */
+}
+
+.avatar-wrapper.is-friend-room {
+  justify-content: space-between;
+}
+
+.avatar {
+  width: 45%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.friend-avatar {
+  /* 친구 아바타의 추가 스타일이 필요한 경우 여기에 추가 */
+}
 </style>
--- Todoup_fe/src/components/room/avatar/BackGround.vue
+++ Todoup_fe/src/components/room/avatar/BackGround.vue
@@ -1,8 +1,11 @@
-<!-- Background.vue -->
 <template>
   <div class="background">
-    <h2>Background</h2>
-    <!-- 배경 내용 -->
+    <div class="avatar-container">
+      <slot name="avatar"></slot>
+    </div>
+    <div class="guest-book">
+      <slot name="guestbook"></slot>
+    </div>
   </div>
 </template>
 
@@ -14,9 +17,28 @@ export default {
 
 <style scoped>
 .background {
+  position: relative;
   width: 100%;
   height: 100vh;
-  background-color: lightblue;
-  position: relative;
+  background-color: #f0f0f0; /* 실제 배경 스타일 */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.avatar-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50%; /* 위쪽 절반을 차지 */
+  border: 1px solid red; /* 구분을 위해 테두리 추가 */
+}
+
+.guest-book {
+  height: 50%; /* 아래쪽 절반을 차지 */
+  border: 1px solid green; /* 구분을 위해 테두리 추가 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
