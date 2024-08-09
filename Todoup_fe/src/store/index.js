126709a73902d4c86a6753ad2f404af75e12cd23
Chany <111185089+chanheek@users.noreply.github.com>
2024-08-09 14:32:04 +0900

    add : vuex-persistedstate

--- Todoup_fe/src/store/index.js
+++ Todoup_fe/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import UserStore from '@/store/user-store';
 import modalStore from './modal-store';
@@ -8,6 +9,12 @@ const store = createStore({
     modal: modalStore,
     user: UserStore,
   },
+  plugins: [
+    createPersistedState({
+      key: 'vuex-user-info', // 로컬 스토리지에 저장될 키 이름
+      paths: ['user.user_info'], // user_info 상태만 저장
+    }),
+  ],
 });
 
 export default store;
