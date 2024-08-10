edc6dfc079c13979f069ae17577d180760406858
Chany <111185089+chanheek@users.noreply.github.com>
2024-08-10 17:51:16 +0900

    fix : FollowService

--- Todoup_be/src/main/java/com/kosa/todoup/service/FollowService.java
+++ Todoup_be/src/main/java/com/kosa/todoup/service/FollowService.java
@@ -1,7 +1,6 @@
 package com.kosa.todoup.service;
 
 import com.kosa.todoup.dto.FollowDTO;
-import com.kosa.todoup.mapper.FollowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +8,4 @@ import java.util.List;
 
 @Service
 public class FollowService {
-    @Autowired
-    private FollowMapper followMapper;
-
-    public List<FollowDTO> findFollowing(long userId) {
-        return followMapper.findFollowing(userId);
-    }
-
-    public List<FollowDTO> findFollowers(long userId) {
-        return followMapper.findFollowers(userId);
-    }
 }
